c6972ec31fa35c732e19027234de9981c9675a59
Eliot <chaosisharmonyofficial@gmail.com>
2020-06-07 00:44:26 +0800

    ARPG three more vids, and updated curriculum file

--- Game Programming/Godot/ARPG/Scripts/Player.gd
+++ Game Programming/Godot/ARPG/Scripts/Player.gd
@@ -6,8 +6,17 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+enum DIR { RIGHT, LEFT, UP, DOWN }
+var curr_dir = DIR.DOWN
+
+onready var animPlayer = $AnimationPlayer
+onready var animTree = $AnimationTree
+onready var animState = animTree.get("parameters/playback")
+
 func _physics_process(delta):
 	process_input(delta)
+	process_movement(delta)
+
 
 func process_input(delta):
 	var input_vector = Vector2.ZERO
@@ -16,8 +25,13 @@ func process_input(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animTree.set("parameters/idle/blend_position", input_vector)
+		animTree.set("parameters/run/blend_position", input_vector)
+		animState.travel("run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animState.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
+
+func process_movement(delta):
 	velocity = move_and_slide(velocity)
