cf9f5684c3e7a1fc6bede976b13f7b5b2aae908e
Eliot <chaosisharmonyofficial@gmail.com>
2020-06-09 23:56:23 +0800

    followed one more vid for ARPG

--- Game Programming/Godot/ARPG/Scripts/Player.gd
+++ Game Programming/Godot/ARPG/Scripts/Player.gd
@@ -9,16 +9,24 @@ var velocity = Vector2.ZERO
 enum DIR { RIGHT, LEFT, UP, DOWN }
 var curr_dir = DIR.DOWN
 
+enum STATE { MOVE, ROLL, ATTACK }
+var state = STATE.MOVE
+
 onready var animPlayer = $AnimationPlayer
 onready var animTree = $AnimationTree
 onready var animState = animTree.get("parameters/playback")
 
+func _ready():
+	animTree.active = true
+
 func _physics_process(delta):
-	process_input(delta)
-	process_movement(delta)
+	match state:
+		STATE.MOVE:	move_state(delta)
+		STATE.ROLL: roll_state(delta)
+		STATE.ATTACK: attack_state(delta)
 
 
-func process_input(delta):
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
@@ -27,11 +35,27 @@ func process_input(delta):
 	if input_vector != Vector2.ZERO:
 		animTree.set("parameters/idle/blend_position", input_vector)
 		animTree.set("parameters/run/blend_position", input_vector)
+		animTree.set("parameters/attack/blend_position", input_vector)
 		animState.travel("run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animState.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
-func process_movement(delta):
+	
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = STATE.ATTACK
+
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animState.travel("attack")
+
+
+func attack_anim_over():
+	state = STATE.MOVE
+
+
+func roll_state(delta):
+	pass
