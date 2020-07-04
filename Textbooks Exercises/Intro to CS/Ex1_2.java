960d2e7b59d22e12177e7e4d8969d8418f101fd2
Eliot <chaosisharmonyofficial@gmail.com>
2020-07-04 21:51:00 +0800

    finished Sedgewick Intro CS 1.2

--- Textbooks Exercises/Intro to CS/Ex1_2.java
+++ Textbooks Exercises/Intro to CS/Ex1_2.java
@@ -1,4 +1,5 @@
 /*************************************************
+*	All use Scanner instead of Command Line arguments
 *
 *	Ex 1.2.2
 *	theta = args[0]
@@ -44,15 +45,39 @@
 *	arguments x and y and prints the Euclidean distance
 *	from the point (x, y) to the origin (0, 0).
 *
+*	Ex 1.2.19
+*	Write a program w/ two int inputs that prints a random
+*	number with their range, inclusive
+*
+*	Ex 1.2.23
+*	Write a program that takes two integer command-line arguments m and
+*	d and prints true if day d of month m is between 3/20 and 6/20, false otherwise.
+*
+*	Ex 1.2.26
+*	Write a program that converts from Cartesian
+* 	to polar coordinates. Your program should accept two double commandline
+* 	arguments x and y and print the polar coordinates r and theta. Use the
+* 	method Math.atan2(y, x) to compute the arctangent value of y/x that is
+* 	in the range from -pi to pi.
+*
+*	Ex 1.2.34 
+*	Write a program that takes three integer command-line arguments
+*	and prints them in ascending order. Use Math.min() and Math.max().
+*
+*	Ex 1.2.35
+*	Write a program to print the instructions for drawing the
+*	dragon curves of order 0 through 5.
 *************************************************/
-
+import java.util.Scanner;
 
 public class Ex1_2
 {
 	public static void main(String[] args)
 	{
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Ex 1.2.2");
-		double theta = Double.parseDouble(args[0]);
+		double theta = sc.nextDouble();
 		System.out.println(Math.sin(theta)*Math.sin(theta)+Math.cos(theta)*Math.cos(theta));
 		
 		// XOR operator for booleans
@@ -101,16 +126,14 @@ public class Ex1_2
 		
 		// input and test
 		System.out.println("\nEx 1.2.14");
-		int x = Integer.parseInt(args[1]);
-		int y = Integer.parseInt(args[2]);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
 		if ((x%y == 0) || (y%x == 0))
 			System.out.println(true);
 		
 		// input and test
 		System.out.println("\nEx 1.2.15");
-		int[] sides = {Integer.parseInt(args[3]),
-					   Integer.parseInt(args[4]),
-					   Integer.parseInt(args[5])};
+		int[] sides = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
 		boolean is_triangle = true;
 		for (int ind = 0; ind < 3; ind++)
 			if ((sides[(ind+1)%3]+sides[(ind+2)%3])<sides[ind])
@@ -126,8 +149,90 @@ public class Ex1_2
 		System.out.println("wrong: " + (G * mass1 * mass2 / r * r));
 		System.out.println("right: " + ((G * mass1 * mass2) / (r * r)));
 
-		// dist from origin
+		// Euclidean dist from origin
 		System.out.println("\nEx 1.2.18");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		System.out.println(Math.sqrt(x*x+y*y));
+		
+		// Random num within range, inclusive
+		System.out.println("\nEx 1.2.19");
+		int lo = 0;
+		int hi = 0;
+		while (hi-lo <= 0)	{ lo = sc.nextInt(); hi = sc.nextInt(); }
+		int range = hi - lo + 1;
+		System.out.println(Math.random()*range+lo);
+		
+		// Check date
+		System.out.println("\nEx 1.2.23");
+		int m = -1;
+		int day = -1;
+		while (m < 1 || m > 12)		m = sc.nextInt();
+		while (day < 1 || day > 31)	day = sc.nextInt();
+		switch (m)
+		{
+			case 3:
+				if (day < 20)	System.out.println(false);
+				else			System.out.println(true);
+				break;
+			case 4:
+				if (day > 30)	System.out.println("INVLAID");
+				else			System.out.println(true);
+				break;
+			case 5:
+				System.out.println(true);
+				break;
+			case 6:
+				if (day > 20)	System.out.println(false);
+				else			System.out.println(true);
+				break;
+			default:
+				System.out.println(false);
+		}
+		
+		// Cartesian to Polar coords
+		System.out.println("\nEx 1.2.26");
+		double x_p = sc.nextDouble();
+		double y_p = sc.nextDouble();
+		r = Math.sqrt(x_p*x_p + y_p*y_p);
+		theta = Math.atan2(y_p, x_p);
+		System.out.println("Radius=" + r + " Theta=" + theta);
+		
+		// Ascending order
+		System.out.println("\nEx 1.2.34");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int[] rank = { Math.max(Math.max(a,b),c), (a==b && a==c) ? a : 0, Math.min(Math.min(a,b),c)};
+		if (rank[0] != a && rank[2] != a)	rank[1] = a;
+		if (rank[0] != b && rank[2] != b)	rank[1] = b;
+		if (rank[0] != c && rank[2] != c)	rank[1] = c;
+		System.out.println("\n"+rank[0]+" "+rank[1]+" "+rank[2]);
+
+		// Dragon curves
+		System.out.println("\nEx 1.2.35");
+		int order = sc.nextInt();
+		while (order < 1 || order > 15)	order = sc.nextInt();
+		System.out.println(build_string(order));
+	}
+	
+	public static String build_string(int N)
+	{
+		String result = "F";
+		for (int i = 0; i < N; i++)
+		{
+			String rev_str = "";
+			for (int j = result.length()-1; j >= 0; j--)
+			{
+				if (result.charAt(j) == 'F') 		rev_str += "F";
+				else if (result.charAt(j) == 'L')	rev_str += "R";
+				else								rev_str += "L";
+			}
+			
+			result += "L" + rev_str;
+		}
+		
+		return result;
 	}
 }
 
