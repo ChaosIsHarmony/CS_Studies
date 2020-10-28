efbdb6f1426905efbc4cc96aaf7b0f12d4e0433b
EC <chaosisharmonyofficial@gmail.com>
2020-10-28 14:45:36 +0800

    Added discrete math textbook exercises and modded TLAP chap 4

--- Textbooks Exercises/Think Like a Programmer/Chap4.cpp
+++ Textbooks Exercises/Think Like a Programmer/Chap4.cpp
@@ -31,6 +31,38 @@ void append(arrayString & s, char c)
 	s = new_s;					// set pointer s to new_s
 }
 
+struct Node
+{
+	int id;
+	int grade;
+	Node * next;
+};
+
+typedef Node * StudentCollection;
+
+void addRecord(StudentCollection& sc, int id, int grade)
+{
+	Node * n = new Node;
+	n->id = id;
+	n->grade = grade;
+	n->next = sc;
+	sc = n;
+}
+
+double average(StudentCollection& sc)
+{
+	if (sc == NULL)	return 0;
+	
+	Node * n = sc;
+	int sum = 0;
+	int len = 0;
+	
+	while(n != NULL)
+	{ len++; sum += n->grade; n = n->next; }
+
+	return double(sum)/len;
+}
+
 int main()
 {
 	// create test string
@@ -47,6 +79,23 @@ int main()
 	// print new
 	std::cout << s << std::endl;
 	
+	// pg. 120 talks about linked lists
+	StudentCollection sc;
+	Node * n1 = new Node, * n2 = new Node, * n3 = new Node;
+	n1->id = 1001; n1->grade = 78; n1->next = n2;
+	n2->id = 1002; n2->grade = 88; n2->next = n3;
+	n3->id = 1003; n3->grade = 98; n3->next = NULL;
+	
+	sc = n1;
+	n1 = n2 = n3 = NULL; 	// these Node pointers are set to null to avoid crosslinking
+				// the next member of each Node is already pointing to the appropriate
+				// locations and if we change the data at n1, n2, or n3 by accident, it would
+				// affect the pointers in each node as well
+
+	addRecord(sc,1004,99);	
+	
+	std::cout << average(sc) << std::endl;
+
 	return 0;
 }
 
