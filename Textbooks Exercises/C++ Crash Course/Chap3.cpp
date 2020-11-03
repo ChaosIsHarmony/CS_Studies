d0c726637a883fa62503d84880a913e48a785dcd
EC <chaosisharmonyofficial@gmail.com>
2020-11-03 23:57:05 +0800

    Modded Chap3 of C++CC; added 1 ex Disc Math; added 3 ex LPTHW

--- Textbooks Exercises/C++ Crash Course/Chap3.cpp
+++ Textbooks Exercises/C++ Crash Course/Chap3.cpp
@@ -8,6 +8,30 @@ struct Animal
 	std::string sound;
 };
 
+struct Rock
+{
+	char roll[5];	// must be +1 longer than intended size to fit NULL
+};
+
+void printAni(Animal* ani_ptr)
+{
+	printf("Pigs definitely go %s\n", ani_ptr->sound.c_str());
+}
+
+void printRock(Rock* rock_ptr, size_t n_rocks)
+{
+	for(size_t i = 0; i < n_rocks; i++)
+		printf("Hi, I'm %s. I'm a lovely rock.\n", rock_ptr[i].roll);
+} 
+
+void changeRock(Rock& rock)
+{
+	rock.roll[0] = 'T';
+	rock.roll[1] = 'u';
+	rock.roll[2] = 'f';
+	rock.roll[3] = 'f';
+}
+
 int main()
 {
 	int sample_int = {};	// sets to zero
@@ -28,6 +52,21 @@ int main()
 	printf("Pigs go %s\n", pig_ptr->sound.c_str());
 	printf("I said, pigs go %s\n", pig.sound.c_str());
 	
+	Animal pigs[] = { "oink!", "Oink!", "OINK!" };
+	printAni(pigs);	// highlights array decaying to a point to arrays first element
+			//  when treated as pointer
+
+	Rock rocks[] = { "Gold", "Mica", "Iron" };
+	printRock(rocks, sizeof(rocks)/sizeof(Rock));
+	Rock* third_rock_1 = &rocks[2];	// two different ways of obtaining a reference to the third element in an array
+	Rock* third_rock_2 = rocks + 2;
+
+	printf("third_rock_add: %p\n", third_rock_1);
+	printf("third_rock_add: %p\n", third_rock_1);
+
+	printf("third_rock: %s\n", third_rock_1->roll);
+	changeRock(rocks[2]);	// pass by reference
+	printf("third_rock: %s\n", third_rock_1->roll);
 
 	return 0;
 }
