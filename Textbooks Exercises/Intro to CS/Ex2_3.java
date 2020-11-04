4a0a97c0d944cb4271dd87f91844a0cb4a27f21f
EC <chaosisharmonyofficial@gmail.com>
2020-11-04 15:29:23 +0800

    added two wx for Intro to CS; added 3 LPTHW files that were actually untracked

--- Textbooks Exercises/Intro to CS/Ex2_3.java
+++ Textbooks Exercises/Intro to CS/Ex2_3.java
@@ -2,8 +2,21 @@
 *	All use Scanner instead of Command Line arguments
 *
 *	Ex 2.3.1
-*	
+*	What happens if you call factorial() with a negative value of n?
+*	With a large value of, say, 35?
+*		- if n < 0, then it goes into an inifinite loop and ends in stackoverflow
+*		- if n is large, then long overflow; if n > 65, then 0, until it reaches 39525
+*
 *	Ex 2.3.3
+*	Give the sequence of integers printed by a call to ex233(6):
+*		public static void ex233(int n)
+*		{
+*			if (n <= 0) return;
+*			StdOut.println(n);
+*			ex233(n-2);
+*			ex233(n-3);
+*			StdOut.println(n);
+*		}
 *
 *	Ex 2.3.4
 *
@@ -29,12 +42,40 @@ public class Ex2_3
 	{
 		sc = new Scanner(System.in);
 		
-		// ???
-		ex_2_3_1();
+		// factorial
+		//ex_2_3_1();
+
+		// recursion trace
+		ex_2_3_3();
 	}
 
 	private static void ex_2_3_1()
 	{
+		// negative
+		//System.out.println(factorial(-3));
+		// large
+		for (int i = 60; i < 1000000000; i++)
+			System.out.println(i + ": " + factorial(i));
+	}
 
-    }
+	public static long factorial(int n)
+	{
+		if (n == 1)	return 1;
+		return n * factorial(n-1);
+	}
+
+	public static void ex_2_3_3()
+	{
+		ex233(6);
+	}
+
+	public static void ex233(int n)
+	{
+		if (n <= 0) return;
+		System.out.println(n);
+		ex233(n-2);
+		ex233(n-3);
+		System.out.println(n);
+	}
+
 }
